40914a6587e6ac8397b78d6a33b8426a4e91aa36
JackBekket <jackatwork@yandex.ru>
2017-02-21 03:01:45 +0300

    started upgrade

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,10 +6,33 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+//import token_artifacts from '../../build/contracts/SimpleToken.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+//var MetaCoin = contract(metacoin_artifacts);
+
+//require syntax.
+// Require our contract artifacts and turn them into usable abstractions.
+var json = require("./build/contracts/SimpleToken.json");
+
+// Turn our contract into an abstraction
+var contract = require("truffle-contract");
+var SimpleToken = contract(json);
+
+// Step 3: Provision the contract with a web3 provider
+//EscrowAdvansed.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+/**
+// Step 4: Use the contract!
+EscrowAdvansed.deployed().then(function(deployed) {
+  return deployed.someFunction();
+});
+**/
+
+
+
+
+
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -39,7 +62,7 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
-      self.refreshBalance();
+  //    self.refreshBalance();
     });
   },
 
@@ -48,6 +71,9 @@ window.App = {
     status.innerHTML = message;
   },
 
+
+//Dummy ----------------------------------
+/**
   refreshBalance: function() {
     var self = this;
 
@@ -84,6 +110,12 @@ window.App = {
       self.setStatus("Error sending coin; see log.");
     });
   }
+**/
+
+
+
+
+
 };
 
 window.addEventListener('load', function() {
@@ -100,3 +132,365 @@ window.addEventListener('load', function() {
 
   App.start();
 });
+
+// old dummy:
+var accounts;
+var account;
+var balance;
+// var tokend;
+ var MyTokenInstance;
+
+ var senderWei;
+ var recipientWei;
+
+ var deci;
+
+ var am_root;
+ var mroot;
+
+
+window.onload = function() {
+// Setting up web3 providers
+  var Web3 = require('web3');
+  // create an instance of web3 using the HTTP provider.
+  // NOTE in mist web3 is already available, so check first if its available before instantiating
+  //var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  //var web3= new Web3();
+
+
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  //Get Accounts
+  web3.eth.getAccounts(function(err, accs) {
+    if (err != null) {
+      alert("There was an error fetching your accounts.");
+      return;
+    }
+
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      return;
+    }
+
+    accounts = accs;
+    account = accounts[0];
+
+
+ $("#transfer_to").val(accounts[1]);
+$("#mint_to").val(accounts[0]);
+
+
+    //Get address
+      var token = MyAdvancedToken.at(MyAdvancedToken.deployed_address);
+
+// var token = MyAdvancedToken.at(0xe2df825a4f1e8c7f40d4bb2fa90654cb368597ba);
+
+    //  console.log(MyAdvancedToken.deployed_address);
+      //console.log(token);
+    //Set adress of deployed contract
+    $("#tokdAddress").html(MyAdvancedToken.deployed_address);
+    //Creating instance
+     myTokenInstance=token;
+
+console.log(myTokenInstance);
+
+
+//Set deci
+deci=3;
+
+//Set rules of transform numbers
+DeciPow(deci);
+
+    //Check Values
+  //  checkValues();
+
+    //Check Total Supply
+    totalSup();
+
+   //refresh Balance
+    refreshBalance();
+
+//refresh All Balance
+// refreshAllBalance ();
+
+
+//deci = myTokenInstance.decimals.call();
+//deci=myTokenInstance.getDecimals.call();
+//console.log('decimals:');
+//console.log(deci);
+
+   });
+//
+
+    //Warmig up UI
+    $("#transfer").click(function() {
+    		var val = $("#transfer_am").val();
+        val=transformIn(val);
+    //    console.log("transfer_val:");
+    //    console.log(val);
+    		var to = $("#transfer_to").val();
+    		sendCoin(to, val);
+    	});
+
+//
+
+   $("#mint").click(function(){
+     var val = $("#mint_am").val();
+     val=transformIn(val);
+     var to = $("#mint_to").val();
+     mintCoin(to, val);
+   });
+
+
+//  var tokenDecl=
+// function(token) {
+//    console.log(token);
+//      myTokenInstance = token;
+  //    checkValues();
+//  };
+//  myTokenInstance = tokenDecl(token);
+//  console.log(myTokenInstance);
+//  tokenDecl(token);
+
+  };
+
+function setStatus(message) {
+//  var status = document.getElementById("status");
+//  status.innerHTML = message;
+  $("#status").html(message);
+};
+
+function setStatusPos(pos, msg){
+$(pos).html(msg);
+
+};
+
+function checkValues() {
+  myTokenInstance.owner.call().then(
+
+    function(organizer) {
+      $("input#tokdOrganizer").val(organizer);
+      return myTokenInstance.getBalance.call(account, {from: account});
+  //    return myTokenInstance.numRegistrants.call();
+  })
+     .then(
+       function(bal) {
+         var be=bal.valueOf();
+         $("#balance").html(be);
+         return myTokenInstance.owner.call();
+
+       });
+}
+
+function refreshBalance(){
+
+ myTokenInstance.getBalance.call(account, {from: account}).then(function(value) {
+   // be = balance_element
+var be=value.valueOf();
+//console.log("be:");
+//console.log(be);
+var be_val;
+be_val=transformOut(be);
+//console.log("be_val");
+//.log(be_val);
+
+// senderWei=web3.toWei(be);
+//console.log('balance:');
+//console.log(be);
+$("#balance").html(be_val);
+//console.log(value);
+//console.log(be);
+}).catch(function(e){
+console.log(e);
+setStatus("Error getting balance; see log.");
+
+});
+}
+
+function difBalance(nacc, numa){
+  myTokenInstance.getBalance.call(nacc, {from: account}).then(function(value) {
+    // be = balance_element
+ var be=value.valueOf();
+
+
+ $(numa).html(be);
+ //console.log(value);
+ //console.log(be);
+ }).catch(function(e){
+ console.log(e);
+ setStatus("Error getting balance; see log.");
+
+ });
+}
+
+function refreshAllBalance(){
+for(var i=0;i<3;i++){
+  myTokenInstance.getBalance.call(accounts[i], {from: account}).then(function(value) {
+    // be = balance_element
+ var be=value.valueOf();
+ var numa= "#bal".i;
+ console.log(numa);
+ $(numa).html(be);
+ //console.log(value);
+ //console.log(be);
+ }).catch(function(e){
+ console.log(e);
+ setStatus("Error getting balance; see log.");
+
+ });
+}
+
+
+
+}
+
+function totalSup(){
+var msg="Инициализация";
+var pos="#totalSup";
+setStatusPos(msg, pos);
+return myTokenInstance.totalSupply.call().then(
+function (sup){
+  val=sup.valueOf();
+  msg=transformOut(val);
+//msg=sup;
+//console.log(sup);
+//console.log(msg);
+setStatusPos(pos, msg);
+
+});
+
+}
+
+//Power to Decimals!
+function DeciPow(deci) {
+   mroot=Math.pow(10,deci);
+   return mroot;
+
+}
+
+
+
+// to Wei
+function transformIn(val) {
+val=val*mroot;
+//console.log("In:");
+//console.log(val);
+return val;
+
+
+}
+
+//from Wei
+function transformOut(val) {
+
+  // var am_prime=val;
+   val=val/mroot;
+//console.log("Out:");
+//.log(val);
+   return val;
+
+
+}
+
+
+ function sendCoin(to, val) {
+//  var meta = MetaCoin.deployed();
+
+
+//console.log(to);
+//console.log(val);
+
+var msg;
+var pos = "#transfer_result";
+var msg_transfer;
+setStatus("Initiating transaction... (please wait)");
+// msg_transfer="Инициализация (пожалуйста,подождите)";
+// $("#transfer_result").html(msg_transfer);
+msg="Инициализация (пожалуйста,подождите)";
+setStatusPos(pos,msg);
+// getBalSenderWei();
+//getBalRecipientWei(to);
+myTokenInstance.transfer(to, val, { from: account}).then(
+  function (){
+   setStatus("Transaction complete!");
+
+  // msg_transfer="Транзакция выполнена";
+  // $("#transfer_result").html(msg_transfer);
+  msg="Транзакция выполнена";
+  setStatusPos(pos,msg);
+   refreshBalance();
+ }).catch(function(e) {
+     console.log(e);
+     setStatus("Error sending coin; see log.");
+    // msg_transfer="Ошибка при отправке, смотри консоль";
+    // $("#transfer_result").html(msg_transfer);
+    msg="Ошибка при отправке, смотри консоль";
+    setStatusPos(pos,msg);
+   });
+  }
+
+function mintCoin(to, val){
+var msg;
+var pos="#mint_result";
+msg="Инициализация, ждите";
+setStatus(msg);
+setStatusPos(pos, msg);
+var cb;  // cb - баланс до чеканки
+
+
+//запрашиваем баланс до чеканки
+ myTokenInstance.getBalance.call(to).then(
+function (prev){
+ cb=prev; //запоминаем старый баланс
+ myTokenInstance.mintToken(to, val, {from:account}); //
+ console.log('val=');
+  console.log(val);
+
+}).then(
+   function(){
+msg="Чеканка";
+  setStatus(msg);
+  setStatusPos(pos, msg);
+  refreshBalance();
+  totalSup();
+}).then(
+
+   function (check){
+    return myTokenInstance.getBalance.call(to); //запрашиваем баланс ПОСЛЕ чеканки
+msg="Проверка";
+    setStatus(msg);
+    setStatusPos(pos,msg);
+    totalSup();
+  //  console.log(check);
+  }).then(
+
+    function(cheked){
+
+  //если новый баланс - старый баланс = значению эмиссии, то эмиссия прошла успешно
+      if(cheked-cb==val||val==0) {
+      msg="Эмиссия прошла успешно";
+      setStatus(msg);
+      setStatusPos(pos,msg);
+      console.log('cb');
+      console.log(cb);
+    //  console.log(check);
+    console.log('cheked');
+      console.log(cheked);
+    } else {
+      msg="Что-то пошло не так";
+      setStatus(msg);
+      setStatusPos(pos,msg);
+      console.log('cb');
+      console.log(cb);
+    //  console.log(check);
+    console.log('cheked');
+      console.log(cheked);
+    }
+  });
+
+}
